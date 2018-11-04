c036f09ecd6ef05d12c765c701fc7af87a36f067
Volodymyr <klymenkodp@gmail.com>
2018-11-03 21:06:37 -0400

    Initial Commit

---
+++ lottie-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lottie-ios-app
+//
+//  Created by Volodymyr Klymenko on 2018-11-03.
+//  Copyright © 2018 Volodymyr Klymenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
